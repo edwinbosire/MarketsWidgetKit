9a898e3d56e2465ce207ddc82720d297f6a303a0
Edwin Bosire <me@edwinb.co.uk>
2025-09-04 21:54:00 +0100

    Initial Commit

---
+++ MarketsWidgetKitTests/MarketsWidgetKitTests.swift
@@ -0,0 +1,16 @@
+//
+//  MarketsWidgetKitTests.swift
+//  MarketsWidgetKitTests
+//
+//  Created by Edwin Bosire on 04/09/2025.
+//
+
+import Testing
+
+struct MarketsWidgetKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
